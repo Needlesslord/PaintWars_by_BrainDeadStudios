0564598bc36e3d73d86dca3f063fb8689ed7ed38
Alex <katyalex1999@gmail.com>
2020-03-29 21:01:36 +0200

    forgot to change a list

--- PaintWars/Project II Game/Motor2D/j1App.cpp
+++ PaintWars/Project II Game/Motor2D/j1App.cpp
@@ -364,7 +364,7 @@ void j1App::SaveGame(const char* file) const
 }
 
 // ---------------------------------------
-void j1App::GetSaveGames(p2List<p2SString>& list_to_fill) const
+void j1App::GetSaveGames(std::list<p2SString>& list_to_fill) const
 {
 	// need to add functionality to file_system module for this to work
 }
