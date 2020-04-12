de3ddb8d6e386fa31810ff20124a1e1abf3d6696
EnricGDV <minenime4thewin@gmail.com>
2020-04-12 18:03:25 +0200

    OnAbove Fixed

--- PaintWars/Project II Game/Motor2D/j1Element.cpp
+++ PaintWars/Project II Game/Motor2D/j1Element.cpp
@@ -37,7 +37,7 @@ bool j1Element::OnAbove()
 	SDL_Point mouse;
 	App->input->GetMousePosition_UI(mouse.x, mouse.y);
 	
-	SDL_Rect intersect = { map_position.x / App->win->scale , map_position.y / App->win->scale, rect.w / App->win->scale, rect.h / App->win->scale };
+	SDL_Rect intersect = { init_map_position.x / App->win->scale , init_map_position.y / App->win->scale, rect.w / App->win->scale, rect.h / App->win->scale };
 
 	if (SDL_PointInRect(&mouse, &intersect) && this->enabled && this->interactable) {
 		if (listener != nullptr)
