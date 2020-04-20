38bb6677c1207b3a1587b4567f4bf7c72e358e45
Nuria Lamonja @needlesslord <needlesslord@gmail.com>
2020-04-20 22:18:25 +0200

    wood producer

--- PaintWars/Project_II_Game/Motor2D/j1Player.cpp
+++ PaintWars/Project_II_Game/Motor2D/j1Player.cpp
@@ -92,15 +92,20 @@ bool j1Player::Update(float dt)
 	p2List_item<j1UIElement*>* UI_List = App->gui->GUI_ELEMENTS.start;
 	while (UI_List != NULL)
 	{
-		LOG("UI COUNT IS %d", z);
-		App->gui->GUI_ELEMENTS[z]->map_position.x = App->gui->GUI_ELEMENTS[z]->init_map_position.x + App->render->camera.x;
-		App->gui->GUI_ELEMENTS[z]->map_position.y = App->gui->GUI_ELEMENTS[z]->init_map_position.y + App->render->camera.y;
+		if (z != 56) {
+			LOG("UI COUNT IS %d", z);
+			App->gui->GUI_ELEMENTS[z]->map_position.x = App->gui->GUI_ELEMENTS[z]->init_map_position.x + App->render->camera.x;
+			App->gui->GUI_ELEMENTS[z]->map_position.y = App->gui->GUI_ELEMENTS[z]->init_map_position.y + App->render->camera.y;
+		}
+		else {
+			App->gui->GUI_ELEMENTS[z]->map_position.x = App->gui->GUI_ELEMENTS[z]->init_map_position.x + App->render->camera.x*0.05;
+			App->gui->GUI_ELEMENTS[z]->map_position.y = App->gui->GUI_ELEMENTS[z]->init_map_position.y + App->render->camera.y*0.05;
+
+		}
 
 
 		UI_List = UI_List->next;
 		++z;
-
-
 	}
 
 	Select_Entities(selector);
