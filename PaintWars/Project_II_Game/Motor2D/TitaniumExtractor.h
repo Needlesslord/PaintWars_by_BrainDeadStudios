7c3120aa58b5d71d97aefb6d8143efd5a035a3fd
Nuria Lamonja @needlesslord <needlesslord@gmail.com>
2020-05-14 13:11:10 +0200

    metal gatherer and titanium extractor

---
+++ PaintWars/Project_II_Game/Motor2D/TitaniumExtractor.h
@@ -0,0 +1,22 @@
+#ifndef __TitaniumExtractor_H__
+#define __TitaniumExtractor_H__
+
+
+#include "Entity.h"
+
+class TitaniumExtractor : public Entity
+{
+public:
+
+	TitaniumExtractor(iPoint tile, int damage, j1Module* listener, Entity* creator);
+	~TitaniumExtractor();
+
+public:
+
+private:
+
+	bool startTimer = true;
+
+};
+
+#endif //__TitaniumExtractor_H__
