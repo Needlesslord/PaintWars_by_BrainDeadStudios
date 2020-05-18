ea819ad0eefc68363062dcb4ec96b47ac575a5a8
EnricGDV <minenime4thewin@gmail.com>
2020-05-18 21:04:43 +0200

    Background Update

--- PaintWars_Source/Source/Motor2D/SettingsScene.cpp
+++ PaintWars_Source/Source/Motor2D/SettingsScene.cpp
@@ -46,13 +46,13 @@ bool SettingsScene::Start()
 	App->scenes->IN_GAME_SCENE = false;
 	backgroundImage = App->gui->AddElement(TypeOfUI::GUI_IMAGE, nullptr, { 0, 0 }, { 0,0 }, true, true, { 0, 0, App->win->width, App->win->width }, nullptr, App->scenes,TEXTURE::MAIN_IMAGE);
 
-	musicLabel = App->gui->AddElement(TypeOfUI::GUI_LABEL, nullptr, { 300, 150 }, { 0, 0 }, false, true, { 0, 0, 0, 0 }, "Music");
+	musicLabel = App->gui->AddElement(TypeOfUI::GUI_LABEL, nullptr, { 300, 150 }, { 0, 0 }, false, true, { 0, 0, 0, 0 }, "Music", App->scenes, TEXTURE::ATLAS, FONT::FONT_MEDIUM_WHITE, 6);
 
-	vfxLabel = App->gui->AddElement(TypeOfUI::GUI_LABEL, nullptr, { 300, 250 }, { 0, 0 }, false, true, { 0, 0, 0, 0 }, "FX");
+	vfxLabel = App->gui->AddElement(TypeOfUI::GUI_LABEL, nullptr, { 300, 250 }, { 0, 0 }, false, true, { 0, 0, 0, 0 }, "FX", App->scenes, TEXTURE::ATLAS, FONT::FONT_MEDIUM_WHITE, 6);
 
-	fullscreenLabel = App->gui->AddElement(TypeOfUI::GUI_LABEL, nullptr, { 300, 350 }, { 0, 0 }, false, true, { 0, 0, 0, 0 }, "Fullscreen");
+	fullscreenLabel = App->gui->AddElement(TypeOfUI::GUI_LABEL, nullptr, { 300, 350 }, { 0, 0 }, false, true, { 0, 0, 0, 0 }, "Fullscreen", App->scenes, TEXTURE::ATLAS, FONT::FONT_MEDIUM_WHITE, 6);
 
-	mutelabel = App->gui->AddElement(TypeOfUI::GUI_LABEL, nullptr, { 300, 450 }, { 0, 0 }, false, true, { 0, 0, 0, 0 }, "Mute");
+	mutelabel = App->gui->AddElement(TypeOfUI::GUI_LABEL, nullptr, { 300, 450 }, { 0, 0 }, false, true, { 0, 0, 0, 0 }, "Mute", App->scenes, TEXTURE::ATLAS, FONT::FONT_MEDIUM_WHITE, 6);
 
 	fxBar= App->gui->AddElement(TypeOfUI::GUI_BUTTON, nullptr, { 620, 255 }, { 0,0 }, true, true, { 785, 57, 268, 26 }, nullptr, App->scenes, TEXTURE::ATLAS);
 	fxBar->hover_rect = { 785, 57, 268, 26 };
